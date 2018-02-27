f0c23783545c4f4b577674e37b084a4cfcd800b8
vikt9ry <timoshenko_vika@mail.ru>
2018-02-27 19:20:39 +0300

    quadratic-eqyation: chernovik_vikt9ry

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function solveEquation(equation) {
-  // your implementation
+module.exports = function solveEquation(equation) { 
+  // your implementation 
+
+  equation = equation.replace(/- /g, "+ -") ;
+  var mas = equation.split(' ') ;
+  var a = mas[0]; 
+  var b = mas[4]; 
+  var c = mas[8]; 
+  
+  var result1 = ((-1*b) + Math.sqrt(Math.pow(b,2) - (4*a*c)))/(2*a); 
+  var result2 =((-1*b) - Math.sqrt(Math.pow(b,2) - (4*a*c)))/(2*a); 
+  
+  return [result2, result1]; 
+  
+
 }
